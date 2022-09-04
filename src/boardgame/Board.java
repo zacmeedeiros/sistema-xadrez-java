2f79aeb2d75589e32c3a39879cb49a5ea15c1b99
Isaac Lopes de Medeiros <isaac.medeiros.704@ufrn.edu.br>
2022-09-04 10:45:16 -0300

    Print do board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,2 +1,37 @@
-package boardgame;public class Board {
+package boardgame;
+
+public class Board {
+    private int fileira;
+    private int coluna;
+    private Piece [][] pieces;
+
+    public Board(int fileira, int coluna) {
+        this.fileira = fileira;
+        this.coluna = coluna;
+        pieces = new Piece[fileira][coluna];
+    }
+
+    public int getFileira() {
+        return fileira;
+    }
+
+    public void setFileira(int fileira) {
+        this.fileira = fileira;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+    public Piece piece(int fileira, int coluna){
+        return pieces [fileira][coluna];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getFileira()][position.getColuna()];
+    }
 }
